cb0390b3e4d440496803bd5312bc501c4e3b38c6
Thomas Aussenac <thomas.aussenac@etu.umontpellier.fr>
2023-10-25 16:45:39 +0200

    Tout fini comment je suis trop fort

--- src/main/java/org/example/SAE_1_AUSSENAC_BENSEDIK_GIARDPELLAT.java
+++ src/main/java/org/example/SAE_1_AUSSENAC_BENSEDIK_GIARDPELLAT.java
@@ -6,7 +6,7 @@ public class SAE_1_AUSSENAC_BENSEDIK_GIARDPELLAT{
 
     //FONCTIONS UTILITAIRES
     //AUSSENAC Thomas
-    public static String afficherTabDouble(double[][] toujoursRatioCypris){
+    public static String afficherTabInt(int[][] toujoursRatioCypris){
         String retour="";
         for(int i=0;i<toujoursRatioCypris.length;i++){
             for(int j=0;j<toujoursRatioCypris[0].length;j++)
@@ -62,28 +62,34 @@ public class SAE_1_AUSSENAC_BENSEDIK_GIARDPELLAT{
 
 
 
-    //FONCTIONS MANCHE 1
 
-    public static void saisieTemps(int[][] tabTemps, int longueurPiste){
+    //FONCTIONS MANCHES
+    public static void saisieRefusChute(int[][] tabTemps, int nbManche){
         for(int i=0;i<tabTemps.length;i++){
-            tabTemps[i][0]=saisieIntMin(0,"Quel temps le numéro "+i+"a t'il fait ? (en millisecondes");
-            if((longueurPiste<600 && tabTemps[i][0]>120000) || (longueurPiste>=600 && tabTemps[i][0]>180000))
-                tabTemps[i][1]=1;
+            if(tabTemps[i][1]==0){
+                if(saisieIntMin(0, "Lors de la "+(nbManche==1 ? "première " : "seconde ")+"manche, combien de refus le numéro "+i+" a-t'il subit ?")>2)
+                    tabTemps[i][1]=1;
+                if(saisieIntMin(0, "Lors de la "+(nbManche==1 ? "première " : "seconde ")+"manche, combien de fois le numéro "+i+" est-il tombé ?")<0)
+                    tabTemps[i][1]=1;
+            }
         }
     }
 
-    public static void saisieRefusChute(int[][] tabTemps){
-        for(int i=0;i<tabTemps.length;i++){
-            if(saisieIntMin(0,"Combien de refus a-t'il subit ?")>2)
-                tabTemps[i][1]=1;
-            if(saisieIntMin(0,"Combien de fois est-il tombé ?")<0)
-                tabTemps[i][1]=1;
-        }
-    }
-
-    public static void saisieNbBarresTombes(int[][] tabTemps,int nbBarres){
+    public static void saisieNbBarresTombes(int[][] tabTemps,int nbBarres, int nbManche){
         for(int i=0;i<tabTemps.length;i++)
-            tabTemps[i][0]+=saisieInt(0,nbBarres,"Combien de barres le numéro"+i+" a-t'il fait tombé ?")*8000;
+            if(tabTemps[i][1]==0){
+                tabTemps[i][0]+=saisieInt(0, nbBarres, "Lors de la "+(nbManche==1 ? "première " : "seconde ")+"manche, combien de barres le numéro"+i+" a-t'il fait tombé ?")*8000;
+            }
+    }
+
+    public static void saisieTemps(int[][] tabTemps, int longueurPiste, int nbManche){
+        for(int i=0;i<tabTemps.length;i++){
+            if(tabTemps[i][1]==0){
+                tabTemps[i][0]=saisieIntMin(0, "Lors de la "+(nbManche==1 ? "première " : "seconde ")+"manche ,quel temps le numéro "+i+"a t'il fait ? (en millisecondes");
+                if((longueurPiste<600 && tabTemps[i][0]>120000) || (longueurPiste>=600 && tabTemps[i][0]>180000))
+                    tabTemps[i][1]=1;
+            }
+        }
     }
 
 
@@ -91,6 +97,56 @@ public class SAE_1_AUSSENAC_BENSEDIK_GIARDPELLAT{
         for(int i=0;i<tabTemps.length;i++){
             System.out.println("Le numéro "+i+" a éffectué un temps de "+tabTemps[i][0]+" et "+(tabTemps[i][1]==1 ? "est éliminé." : "passe à la deuxième manche !"));
         }
+    }
+
+    //PODIUM
+
+    public static void afficherPodium(int[][] tabTemps){
+        int nb1 = 180001,nb2 = 180001,nb3 = 180001, compteur=0;
+        String retour="A la première place ce trouve : ";
+        for(int i=0;i<tabTemps.length;i++){
+            if(tabTemps[i][0]<nb3){
+                if(tabTemps[i][0]<nb2){
+                    if(tabTemps[i][0]<nb1){
+                        nb3=nb2;
+                        nb2=nb1;
+                        nb1=tabTemps[i][0];
+                    }else {
+                        nb3=nb2;
+                        nb2=tabTemps[i][0];
+                    }
+                }else {
+                    nb3=tabTemps[i][0];
+                }
+            }
+        }
+        for(int i=0;i<tabTemps.length;i++){
+            if(tabTemps[i][0]==nb1){
+                retour+=i+" ";
+                compteur++;
+            }
+        }
+        if(compteur<3){
+            retour+="\nA la seconde place ce trouve : ";
+            for(int i=0;i<tabTemps.length;i++){
+                if(tabTemps[i][0]==nb2){
+                    retour+=i+" ";
+                    compteur++;
+                }
+            }
+        }
+        if(compteur<3){
+            retour+="\nA la troisième place ce trouve : ";
+            for(int i=0;i<tabTemps.length;i++){
+                if(tabTemps[i][0]==nb3){
+                    retour+=i+" ";
+                }
+            }
+        }
+        System.out.println(nb1);
+        System.out.println(nb2);
+        System.out.println(nb3);
+        System.out.println(retour);
     }
 
 
@@ -101,20 +157,48 @@ public class SAE_1_AUSSENAC_BENSEDIK_GIARDPELLAT{
 
     public static void main(String[] args){
         //SAISIE POUR EPREUVE
+        /*
         int[][] tabTemps=saisieNbParticipants();
         int longueurPiste=saisieTailleCourse();
         int nbObstacles=saisieNbObstacle(longueurPiste);
         int nbBarres=saisieNbBarres(nbObstacles);
 
         //SAISIE MANCHE 1
-        saisieTemps(tabTemps, longueurPiste);
-        saisieRefusChute(tabTemps);
-        saisieNbBarresTombes(tabTemps, nbBarres);
+        System.out.println("Début de la première mannche.");
+        saisieTemps(tabTemps, longueurPiste,1);
+        saisieRefusChute(tabTemps,1);
+        saisieNbBarresTombes(tabTemps, nbBarres,1);
 
         //AFFICHAGE RESULTATS MANCHE 1
         afficheResultatsManche1(tabTemps);
 
         //SAISIE MANCHE 2
+        System.out.println("Début de la seconde mannche.");
+        saisieTemps(tabTemps, longueurPiste,2);
+        saisieRefusChute(tabTemps,2);
+        saisieNbBarresTombes(tabTemps, nbBarres,2);
+        */
+
+        int[][] test=new int[15][2];
+        for(int i=0;i<test.length;i++)
+            test[i][0]=(int)  (Math.random()*10000);
+        test[5][0]=150;
+        test[6][0]=150;
+        test[7][0]=150;
+        test[8][0]=150;
+
+        System.out.println(afficherTabInt(test));
+
+        afficherPodium(test);
+
+
+
+
+
+
+
+
+
 
     }
 }
